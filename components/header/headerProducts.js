2b31b19fce2360412d40be6a8f290f4759a9d8ed
devops3safelab <devops3safelabs@gmail.com>
2023-04-13 13:52:56 +0000

    Landing Animation

--- components/header/headerProducts.js
+++ components/header/headerProducts.js
@@ -2,30 +2,57 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { useEffect, useState } from "react";
 
 function headerProducts({imageStyle}) {
-  const images=['fish.png', 'fish-1.png', 'fish-2.png', 'fish-3.png'];
-  const [animatedFish, setAnimatedFish] = useState(images[0]);
+  const heroProducts=[
+    {id:1, name:'Fish 1', image:'fish.png', price:10},
+    {id:2, name:'Fish 2', image:'fish-1.png', price:126},
+    {id:3, name:'Fish 3', image:'fish-2.png', price:31},
+    {id:4, name:'Fish 4', image:'fish-3.png', price:31},
+  ];
+  const [animatedFish, setAnimatedFish] = useState(heroProducts[0]);
+  
   // useEffect(() => {
   //   console.log(imageStyle);
   // }, [imageStyle]);
 
 
   const changeAnimatedFish=()=>{
-    if(animatedFish!=images[images.length - 1]){
-      let index= images.findIndex(img => img==animatedFish);
-      setAnimatedFish(images[index + 1]);
+    if(animatedFish.image!=heroProducts[heroProducts.length - 1].image){
+      let index= heroProducts.findIndex(p => p.image==animatedFish.image);
+      setAnimatedFish(heroProducts[index + 1]);
     }else{
-      setAnimatedFish(images[0]);
+      setAnimatedFish(heroProducts[0]);
     }
   }
-  setInterval(changeAnimatedFish, 5000)
+  setInterval(changeAnimatedFish, 10000)
 
-  const animate={
-    x:[-2000, -50, 2000],
-    y:[500, -100, -300],
-    rotate:[0, -15, 10],
+  const fishAnimation={
+    x:[-2000, -60, -60, -60, 2000],
+    y:[500, -100, -100, -100, -300],
+    display:'block',
+    rotate:[0, -13, -15, -13, 10],
     transition:{
-      duration:5,
-      repeat:Infinity
+      duration:10,
+      repeat:1,
+    },
+  }
+
+  const priceAnimation={
+    x:[2000, 200, 200, 200, -2000],
+    y:[100,  100, 100],
+    display:'flex',
+    transition:{
+      duration:10,
+      repeat:1,
+    },
+  }
+
+  const textAnimation={
+    x:[-2000, -250, -250, -250, 2000],
+    y:[-200,  -200, -200],
+    display:'flex',
+    transition:{
+      duration:10,
+      repeat:1,
     },
   }
   
@@ -43,17 +70,33 @@ function headerProducts({imageStyle}) {
           lou pescadou
         </motion.h1>
       </div>
-      <div className="w-5/6 h-2/6 flex justify-center items-start">
-        {images.map(image=>(
-          <motion.img
-          key={image}
-            transition={{ duration: 2 }}
-            src={`/assets/${image}`}
-            className="5/6 md:w-3/6 absolute"
-            alt=""
-            animate={image==animatedFish? animate : {}}
-            style={{x:2000, y:500}}
-          />
+      <div className="w-5/6 h-2/6 flex justify-center items-start w-1/2">
+        <motion.div className="glassmorphism"></motion.div>
+        {heroProducts.map(product=>(
+            <>
+              <motion.h2
+                animate={product.image==animatedFish.image? textAnimation : {}}
+                style={{x:2000, y:500, display:'none'}}
+                className="fish-text"
+              >
+                {product.name}
+              </motion.h2>
+              <motion.img
+                transition={{ duration: 2 }}
+                src={`/assets/${product.image}`}
+                className="5/6 md:w-3/6 absolute fish"
+                alt=""
+                animate={product.image==animatedFish.image? fishAnimation : {}}
+                style={{x:2000, y:500, display:'none'}}
+              />
+              <motion.h3
+                animate={product.image==animatedFish.image? priceAnimation : {}}
+                style={{x:2000, y:500, display:'none'}}
+                className="fish-price"
+              >
+                {product.price}
+              </motion.h3>
+            </>
         ))}
       </div>
       <div className="w-5/6 h-1/6 flex justify-end items-start">
